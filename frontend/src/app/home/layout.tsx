c56fbc1cc6e2ac020fa2791e65396603cee35add
Nathan-C-Wong <nathanbusiness55555@gmail.com>
2024-04-20 15:45:32 -0700

    Started working on messages

--- frontend/src/app/home/layout.tsx
+++ frontend/src/app/home/layout.tsx
@@ -114,28 +114,45 @@ export default function HomeLayout({ children }: Readonly<{children: React.React
             <div className="h-full px-3 pb-4 overflow-y-none bg-whit">
                 <ul className="space-y-2 font-medium">
                     <li>
-                        <Link href="/home/timeline" onClick={toggleSidebar} className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group">
-                            <img src="/img/home.png" className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900" />
+                        <Link href="/home/timeline" onClick={toggleSidebar}
+                              className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group">
+                            <img src="/img/home.png"
+                                 className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900"/>
                             <span className="flex-1 ms-3 whitespace-nowrap">Home</span>
                         </Link>
                     </li>
                     <li>
-                        <Link href="/home/search" onClick={toggleSidebar} className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group">
-                            <img src="/img/search.png" className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900" />
+                        <Link href="/home/search" onClick={toggleSidebar}
+                              className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group">
+                            <img src="/img/search.png"
+                                 className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900"/>
                             <span className="flex-1 ms-3 whitespace-nowrap">Search</span>
                         </Link>
                     </li>
                     <li>
-                        <Link href="/home/notifications" onClick={toggleSidebar} className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group">
-                            <img src="/img/notification.png" className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900" />
+                        <Link href="/home/notifications" onClick={toggleSidebar}
+                              className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group">
+                            <img src="/img/notification.png"
+                                 className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900"/>
                             <span className="flex-1 ms-3 whitespace-nowrap">Notifications</span>
-                            <span className="inline-flex items-center justify-center w-3 h-3 p-3 ms-3 text-sm font-medium text-blue-500 bg-blue-100 rounded-full">69</span>
+                            <span
+                                className="inline-flex items-center justify-center w-3 h-3 p-3 ms-3 text-sm font-medium text-blue-500 bg-blue-100 rounded-full">69</span>
                         </Link>
                     </li>
                     <li>
-                        <Link href="/home/profile/me" onClick={toggleSidebar} className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group">
-                            <img src="/img/profile.png" className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900" />
+                        <Link href="/home/profile/me" onClick={toggleSidebar}
+                              className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group">
+                            <img src="/img/profile.png"
+                                 className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900"/>
                             <span className="flex-1 ms-3 whitespace-nowrap">My Profile</span>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/home/messages" onClick={toggleSidebar}
+                              className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group">
+                            <img src="/img/messages.png"
+                                 className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900"/>
+                            <span className="flex-1 ms-3 whitespace-nowrap">Messages</span>
                         </Link>
                     </li>
                 </ul>
@@ -143,11 +160,11 @@ export default function HomeLayout({ children }: Readonly<{children: React.React
             </aside>
 
             <div className="p-4 sm:ml-64">
-            <div className="p-4 mt-14">
-                <div className="block">
-                    {children}
+                <div className="p-4 mt-14">
+                    <div className="block">
+                        {children}
+                    </div>
                 </div>
-            </div>
             </div>
         </div>
     );
