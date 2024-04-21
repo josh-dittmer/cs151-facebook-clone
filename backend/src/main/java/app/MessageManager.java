f06b2d3b92de436033dd90ff5889b000d5c9b883
Nathan-C-Wong <nathanbusiness55555@gmail.com>
2024-04-21 12:19:46 -0700

    Second commit for messages * application class added getter * DB connection class new table for messages (unused atm) * message manager is wip * also need to do frontend later for messages and notifications which might have a similar structure

--- backend/src/main/java/app/MessageManager.java
+++ backend/src/main/java/app/MessageManager.java
@@ -3,16 +3,19 @@ package app;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class MessageManager {
     private static final Logger log = LoggerFactory.getLogger(MessageManager.class);
     private Application app;
     public MessageManager(Application app) {
         this.app = app;
     }
-
-    //Functionality: send message, delete message, read message from entire chat
-    //Also when clicking on messages should open up a list of past messages with all people you messaged
-
 
 
 
