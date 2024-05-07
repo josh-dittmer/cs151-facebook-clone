bbc9458cd97453c9c5ddaf38535b607c127de8e6
joseph <joseph.hawkins@sjsu.edu>
2024-05-06 20:32:53 -0700

    notif

--- frontend/src/app/home/components/notification.tsx
+++ frontend/src/app/home/components/notification.tsx
@@ -9,6 +9,10 @@ const NotificationComponent: React.FC<NotificationProps> = ({ notification }) =>
         <div>
             {/* Render notification content here */}
             <p>{notification.action}</p>
+            <div className="bg-gray-200 p-4 rounded-lg">
+                <p>This is a big container under the notification content.</p>
+                <p>It can contain additional information or actions related to the notification.</p>
+            </div>
         </div>
     );
 };
